e0b2d0fcbde1eff96440c9733fd298c9aeb17a60
António Barros (1200606) <1200606@isep.ipp.pt>
2021-05-30 18:17:10 +0100

    L2S20-88 implementaction of a file sh for run of the server

---
+++ run-activityflowengine.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#REM set the class path,
+#REM assumes the build was executed with maven copy-dependencies
+
+export BASE_CP=base.core/target/base.core-1.3.0-SNAPSHOT.jar:base.core/target/dependency/*;
+
+#REM call the java VM, e.g,
+# eapli.base.app.backoffice.console.BaseBackoffice
+java -cp $BASE_CP eapli.base/Dashboardmanagement.TCPSrvMFA
