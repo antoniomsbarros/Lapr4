a7298a82ba6d497b60e1bdc0ae59e7d2b47154c6
Jose Teixeira <1150462@isep.ipp.pt>
2021-06-01 15:51:29 +0100

    L2S20-75 #application  taskmanagement/ application/AddManualTaskController refinement

--- base.core/src/main/java/eapli/base/taskmanagement/application/AddManualTaskController.java
+++ base.core/src/main/java/eapli/base/taskmanagement/application/AddManualTaskController.java
@@ -1,12 +1,14 @@
 package eapli.base.taskmanagement.application;
 
 import eapli.base.clientusermanagement.domain.ClientUser;
+import eapli.base.clientusermanagement.repositories.ClientUserRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.ordermanagement.domain.Attribute;
 import eapli.base.taskmanagement.domain.*;
 import eapli.base.taskmanagement.repositories.AutomaticTaskRepository;
 import eapli.base.taskmanagement.repositories.ManualTaskRepository;
 import eapli.base.teamManagement.domain.TeamType;
+import eapli.base.teamManagement.repositories.TeamRepository;
 import eapli.base.usermanagement.domain.BaseRoles;
 import eapli.framework.general.domain.model.Description;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
@@ -19,14 +21,15 @@ public class AddManualTaskController {
     private final AuthorizationService authz = AuthzRegistry.authorizationService();
     private final ManualTaskRepository manualTaskRepository = PersistenceContext.repositories().manualTasks();
 
+    private final TeamRepository teamRepository = PersistenceContext.repositories().team();
+
     public void addManualTask(Calendar deadline, Integer priority,
-                                 ClientUser collaborator, Set<Attribute>commentaryList, Set<Attribute>decisionList ) {
-        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.POWER_USER, BaseRoles.GSH_MANAGER);
+                                 ClientUser collaborator, Description commentary, Description decision ) {
+        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.POWER_USER, BaseRoles.ADMIN,BaseRoles.GSH_MANAGER);
 
         try {
-            final ManualTask manualTask = new ManualTask(TaskType.UNKNOWN, TaskState.PENDING, new Deadline(deadline),
-                                                         priority,collaborator,commentaryList,decisionList);
-
+            final ManualTask manualTask = new ManualTask(TaskState.PENDING, new Deadline(deadline),
+                                                         priority,TaskType.UNKNOWN,collaborator,commentary,decision);
             manualTaskRepository.save(manualTask);
         } catch (IllegalArgumentException e) {
             System.out.println(e);
@@ -34,5 +37,15 @@ public class AddManualTaskController {
 
         System.out.println("\nAutomaticTask saved!");
 
+    }
+
+    public void choosetaskType(TaskType t, ManualTask task){
+            switch (t){
+                case UNKNOWN:
+                    break;
+                default:
+                    task.setType(t);
+                    break;
+            }
     }
 }
