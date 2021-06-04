3d7890ee2637d79556002a72bd99c427b0ba8526
António Barros(1200606) <1200606@isep.ipp.pt>
2021-06-04 11:56:20 +0100

    fix a bug on the server about authorization service

--- base.core/src/main/java/eapli/base/Dashboardmanagement/HttpServerAjaxVoting.java
+++ base.core/src/main/java/eapli/base/Dashboardmanagement/HttpServerAjaxVoting.java
@@ -29,7 +29,7 @@ public class HttpServerAjaxVoting {
         System.out.println(Arrays.toString(activitysremaningbypriority));
 
 
-        activityResolucao[0]="ActivitY N: 1 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";
+        /*activityResolucao[0]="ActivitY N: 1 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";
         activityResolucao[1]="ActivitY N: 2 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";
         activityResolucao[2]="ActivitY N: 3 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";
         allactivitys=new String[3];
@@ -39,7 +39,7 @@ public class HttpServerAjaxVoting {
         activitysremaningbypriority=new String[3];
         activitysremaningbypriority[0]="ActivitY N: 1 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";
         activitysremaningbypriority[1]="ActivitY N: 2 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";
-        activitysremaningbypriority[2]="ActivitY N: 3 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";
+        activitysremaningbypriority[2]="ActivitY N: 3 Priority: 1 DeadLine: Fri Nov 15 07:59:52 GMT 2030";*/
 	try { sock = new ServerSocket(70); }
 	catch(IOException ex) {
             System.out.println("Server failed to open local port " + 70);
--- base.core/src/main/java/eapli/base/catalogmanagement/application/SearchActivity.java
+++ base.core/src/main/java/eapli/base/catalogmanagement/application/SearchActivity.java
@@ -3,18 +3,21 @@ package eapli.base.catalogmanagement.application;
 import eapli.base.catalogmanagement.domain.Activity;
 import eapli.base.catalogmanagement.repository.ActivityRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.usermanagement.domain.BasePasswordPolicy;
+import eapli.framework.application.UseCaseController;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.domain.model.PlainTextEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@UseCaseController
 public class SearchActivity {
     private final AuthorizationService authz;
     private final ActivityRepository activityRepository;
 
     public SearchActivity() {
-        this.authz = AuthzRegistry.authorizationService();;
+        this.authz= AuthzRegistry.authorizationService();
         this.activityRepository = PersistenceContext.repositories().activity();
     }
     public List<Activity> prepareactivity(Integer collaboratorNumber){
