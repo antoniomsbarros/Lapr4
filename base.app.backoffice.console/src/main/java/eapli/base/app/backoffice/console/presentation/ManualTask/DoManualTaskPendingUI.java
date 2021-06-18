6e302e34f16975289d0f5b2176d29694fc38d6e0
Marly Pimpita <1191444@isep.ipp.pt>
2021-06-18 17:03:28 +0100

    L2S20-113 class UI implementation

---
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/ManualTask/DoManualTaskPendingUI.java
@@ -0,0 +1,94 @@
+package eapli.base.app.backoffice.console.presentation.ManualTask;
+
+import eapli.base.app.backoffice.console.presentation.clientuser.collaboratorDTOListPrinter;
+import eapli.base.app.backoffice.console.presentation.clientuser.teamDTOListPrinter;
+import eapli.base.clientusermanagement.dto.ClientUserDTO;
+import eapli.base.ordermanagement.domain.Form;
+import eapli.base.taskmanagement.application.DoManualTaskPendingController;
+import eapli.base.taskmanagement.domain.ManualTask;
+import eapli.base.taskmanagement.domain.TaskType;
+import eapli.base.teamManagement.dto.TeamDTO;
+import eapli.framework.io.util.Console;
+import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.SelectWidget;
+
+/**
+ *
+ * @author marly
+ */
+public class DoManualTaskPendingUI extends AbstractUI {
+    private final DoManualTaskPendingController controller = new DoManualTaskPendingController();
+    private boolean opValida = false;
+    private SelectWidget<ManualTask> manualTaskSelectWidget;
+
+    @Override
+    protected boolean doShow() {
+        Form form;
+        do {
+            System.out.println("Search by:");
+            System.out.println("1.Approval");
+            System.out.println("2.Resolution");
+            System.out.println("0. Exit");
+
+            final int op = Console.readInteger("Search Option: ");
+            switch (op){
+                case 0: return true;
+                case 1:
+                    manualTaskSelectWidget = new SelectWidget<>("Select the Approval Manual Task:",
+                            controller.manualTaskPendingList(TaskType.APPROVAL), new ManualTaskPrinter());
+                    if (controller.manualTaskPendingList(TaskType.APPROVAL).iterator().hasNext()){
+                        manualTaskSelectWidget.show();
+                        try {
+                            ManualTask manualTask = manualTaskSelectWidget.selectedElement();
+                            System.out.println("Manual Task: " + manualTask.identity());
+                            form = manualTask.Form();
+
+                            //Complete From
+
+                            controller.doManualTaskPending(manualTask);
+                        }catch (NullPointerException e){
+                            System.out.println("Invalid Option: " + e);
+                        }
+                    }
+                    else {
+                        System.out.println("Without Approval Manual Task!");
+                    }
+
+                    opValida = true;
+                    break;
+                case 2:
+
+                    manualTaskSelectWidget = new SelectWidget<>("Select the Resolution Manual Task:",
+                            controller.manualTaskPendingList(TaskType.RESOLUTION), new ManualTaskPrinter());
+                    if (controller.manualTaskPendingList(TaskType.RESOLUTION).iterator().hasNext()){
+                        manualTaskSelectWidget.show();
+                        try {
+                            ManualTask manualTask = manualTaskSelectWidget.selectedElement();
+                            System.out.println("Manual Task: " + manualTask.identity());
+                            form = manualTask.Form();
+
+                            //Complete From
+
+                            controller.doManualTaskPending(manualTask);
+                        }catch (NullPointerException e){
+                            System.out.println("Invalid Option: " + e);
+                        }
+                    }
+                    else {
+                        System.out.println("Without Resolution Manual Task!");
+                    }
+
+                    opValida = true;
+                    break;
+                default: opValida = false;
+            }
+        }while (!opValida);
+
+        return false;
+    }
+
+    @Override
+    public String headline() {
+        return "Do Manual Task Pending";
+    }
+}
