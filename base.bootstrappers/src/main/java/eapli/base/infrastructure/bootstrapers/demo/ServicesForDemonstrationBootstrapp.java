62b7e89d83600bf95249461a64e46ecf7dedee00
Daniel Reis <1200608@isep.ipp.pt>
2021-06-20 21:34:25 +0100

    L2S20-108 Modifications on Service Bootstrap

--- base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/ServicesForDemonstrationBootstrapp.java
+++ base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/ServicesForDemonstrationBootstrapp.java
@@ -44,6 +44,12 @@ public class ServicesForDemonstrationBootstrapp implements Action {
                 Description.valueOf("ex1"), Description.valueOf("S_Blue"),
                 catalogs().get(1),"y","ex1","ex1","ex1", clientUsers);
 
+        //Exemplo 2
+        createCatalog(Description.valueOf("Titulo Servico Exemplo 2"),Description.valueOf("Descricao Curta Servico Exemplo 2"),
+                Description.valueOf("Descricao Longa Servico Exemplo 2"),Description.valueOf("Icone Servico Exemplo 2"),
+                teams,clientUsers.get(0));
+        createExampleService2(Description.valueOf("Autorização para Aplicação de Desconto"),Description.valueOf("ex2"),Description.valueOf("ex2"),Description.valueOf("S_Red"),
+                catalogs().get(2),"y","ex2","ex2","ex2",clientUsers);
 
         return true;
     }
@@ -64,33 +70,33 @@ public class ServicesForDemonstrationBootstrapp implements Action {
         createServiceController.checkIfServiceIsComplete(title,smalldescription,fulldescription,icon,enableFeedback);
 
         createServiceController.addAttribute(Long.valueOf("1"),  Description.valueOf("desc1"),  Description.valueOf("Período de ausência (data inicio)"),  Description.valueOf("label"),
-                Description.valueOf("YEAR:[2][0][0-9][0-9];\nMONTH:[0][1-9]|[1][0-2];\nDAY:[0][1-9]|[1][0-9]|[2][0-8]|[2][9]|[3][0-1];"), TypeofData.valueOf("String"));
-        createServiceController.addAttribute(Long.valueOf("2"),  Description.valueOf("desc2"),  Description.valueOf("Período de ausência (data fim)"),  Description.valueOf("label2"),  Description.valueOf("reg2"), TypeofData.valueOf("String"));
-        createServiceController.addAttribute(Long.valueOf("3"),  Description.valueOf("desc3"),  Description.valueOf("Tipo Ausencia"),  Description.valueOf("label3"),  Description.valueOf("'Justificada'|'Ferias'|'Nao Justificada'"), TypeofData.valueOf("String"));
-        createServiceController.addAttribute(Long.valueOf("4"),  Description.valueOf("desc4"),  Description.valueOf("Justificação"),  Description.valueOf("label4"),  Description.valueOf("[a-zA-Z]+' '*"), TypeofData.valueOf("String"));
+                Description.valueOf("((0[1-9]|1[0-9]|2[0-9]|3[0-1])\\/(01|0[3-9]|1[0-2])\\/(20[0-9][0-9]))|((0[1-9]|1[0-9]|2[0-8]|29)\\/02\\/(20[0-9][0-9]))"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("2"),  Description.valueOf("desc2"),  Description.valueOf("Período de ausência (data fim)"),  Description.valueOf("label2"),  Description.valueOf("((0[1-9]|1[0-9]|2[0-9]|3[0-1])\\/(01|0[3-9]|1[0-2])\\/(20[0-9][0-9]))|((0[1-9]|1[0-9]|2[0-8]|29)\\/02\\/(20[0-9][0-9]))"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("3"),  Description.valueOf("desc3"),  Description.valueOf("Tipo Ausencia"),  Description.valueOf("label3"),  Description.valueOf("(Ferias|Justificada|Nao Justificada)"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("4"),  Description.valueOf("desc4"),  Description.valueOf("Justificação"),  Description.valueOf("label4"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
 
         createServiceController.saveForm(Description.valueOf(formName),Description.valueOf(scriptName), pedido);
         Calendar date = Calendar.getInstance();
         date.set(2022,2,2);
         //Task1
         formController = new CreateFormController();
-        formController.addAttribute(Long.valueOf("5"),  Description.valueOf("d"),  Description.valueOf("Decisao"),  Description.valueOf("l"),  Description.valueOf("r"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("6"),  Description.valueOf("d2"),  Description.valueOf("Tento fundamentando a decisao"),  Description.valueOf("l2"),  Description.valueOf("r2"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("5"),  Description.valueOf("d"),  Description.valueOf("Decisao"),  Description.valueOf("l"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("6"),  Description.valueOf("d2"),  Description.valueOf("Tento fundamentando a decisao"),  Description.valueOf("l2"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
         Form at1 = formController.saveForm(Description.valueOf("ex2"),Description.valueOf("script2"),aprovacao);
         Responsable responsable1 = new Responsable(Long.valueOf("1"), lstCollabs.get(0), new Delegaction(Long.valueOf("1"), Description.valueOf("Justificacao"), Designation.valueOf("Alternativa")), teams().get(0));
         Task t1 = addManualTaskController.addManualTask(new Deadline(date), 3,responsable1 ,Description.valueOf("Comentarioex1"), Description.valueOf("Decisaoex1"), at1, new ArrayList<>());
         //Task 2
         formController = new CreateFormController();
         formController.addAttribute(Long.valueOf("7"),  Description.valueOf("d"),  Description.valueOf("Dias de férias já gozados no ano"),  Description.valueOf("l"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("8"),  Description.valueOf("d2"),  Description.valueOf("Dias de férias gozados do período solicitado"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("9"),  Description.valueOf("d3"),  Description.valueOf("Dias de férias totais"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("10"),  Description.valueOf("d4"),  Description.valueOf("Dias de falta justificadas já ocorridas no ano"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("11"),  Description.valueOf("d5"),  Description.valueOf("Dias de faltas justificadas do período solicitado"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("12"),  Description.valueOf("d6"),  Description.valueOf("Dias de faltas justificadas totais"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("13"),  Description.valueOf("d7"),  Description.valueOf("Dias de falta não justificadas já ocorridas no ano"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("14"),  Description.valueOf("d8"),  Description.valueOf("Dias de faltas não justificadas do período solicitado"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("15"),  Description.valueOf("d9"),  Description.valueOf("Dias de faltas não justificadas totais"),  Description.valueOf("l2"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
-        formController.addAttribute(Long.valueOf("16"),  Description.valueOf("d10"),  Description.valueOf("Comentario"),  Description.valueOf("l2"),  Description.valueOf("[A-Za-z]+"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("8"),  Description.valueOf("d2"),  Description.valueOf("Dias de férias gozados do período solicitado"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("9"),  Description.valueOf("d3"),  Description.valueOf("Dias de férias totais"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("10"),  Description.valueOf("d4"),  Description.valueOf("Dias de falta justificadas já ocorridas no ano"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("11"),  Description.valueOf("d5"),  Description.valueOf("Dias de faltas justificadas do período solicitado"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("12"),  Description.valueOf("d6"),  Description.valueOf("Dias de faltas justificadas totais"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("13"),  Description.valueOf("d7"),  Description.valueOf("Dias de falta não justificadas já ocorridas no ano"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("14"),  Description.valueOf("d8"),  Description.valueOf("Dias de faltas não justificadas do período solicitado"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("15"),  Description.valueOf("d9"),  Description.valueOf("Dias de faltas não justificadas totais"),  Description.valueOf("l2"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("16"),  Description.valueOf("d10"),  Description.valueOf("Comentario"),  Description.valueOf("l2"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
         Form at2 = formController.saveForm(Description.valueOf("ex3"),Description.valueOf("script3"),resolucao);
 
         Responsable responsable2 = new Responsable(Long.valueOf("2"), lstCollabs.get(1), new Delegaction(Long.valueOf("2"), Description.valueOf("Justificacao2"), Designation.valueOf("Alternativa2")), teams().get(0));
@@ -105,6 +111,66 @@ public class ServicesForDemonstrationBootstrapp implements Action {
         Task automaticTask= createServiceController.saveAutomaticTask(date, 2, "C:/ola/ola.exe");
         formController = new CreateFormController();
         formController.addAttribute(Long.valueOf("17"),  Description.valueOf("d"),  Description.valueOf("Dias de férias já gozados no ano"),  Description.valueOf("l"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
+        Form at3 = formController.saveForm(Description.valueOf("ex3"),Description.valueOf("script3"),resolucao);
+
+
+        map.put(Long.valueOf("3"), automaticTask);
+
+        lstSeq = createServiceController.saveSequence(map);
+        Workflow workflow = createServiceController.saveWorkflow(lstSeq);
+        createServiceController.saveService(workflow);
+
+    }
+
+    public void createExampleService2(Description title,Description smalldescription, Description fulldescription,
+                                      Description icon, Catalog choosenCatalog,String enableFeedback,String keyword,String formName, String scriptName, List<ClientUser> lstCollabs){
+
+        String pedido ="REQUEST";
+        String aprovacao ="APROVATION";
+        String resolucao ="RESOLUTION";
+
+        this.createServiceController = new CreateServiceController();
+        createServiceController.createService(title,smalldescription,fulldescription,icon,choosenCatalog);
+
+        createServiceController.addKeyword(new Keyword(keyword));
+        createServiceController.addKeywordListToService();
+        createServiceController.setFeedback(enableFeedback);
+
+        createServiceController.checkIfServiceIsComplete(title,smalldescription,fulldescription,icon,enableFeedback);
+
+        createServiceController.addAttribute(Long.valueOf("40"),  Description.valueOf("a40"),  Description.valueOf("Código Interno Cliente"),  Description.valueOf("label40"),
+                Description.valueOf("[B-DF-HJ-NP-TV-Z]{3}[0-9]{3}"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("41"),  Description.valueOf("a41"),  Description.valueOf("Nome"),  Description.valueOf("label41"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("42"),  Description.valueOf("a42"),  Description.valueOf("Tipo de Desconto"),  Description.valueOf("label42"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("43"),  Description.valueOf("a43"),  Description.valueOf("Recorrência"),  Description.valueOf("label43"),  Description.valueOf("(Unica|Ate Data Limite)"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("44"),  Description.valueOf("a44"),  Description.valueOf("Percentagem de Desconto"),  Description.valueOf("label44"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("45"),  Description.valueOf("a45"),  Description.valueOf("Valor de Desconto"),  Description.valueOf("label45"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("46"),  Description.valueOf("a46"),  Description.valueOf("Identificação da Fatura"),  Description.valueOf("label46"),  Description.valueOf("[a-zA-Z0-9]+"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("47"),  Description.valueOf("a47"),  Description.valueOf("Data Limite"),  Description.valueOf("label47"),  Description.valueOf("((0[1-9]|1[0-9]|2[0-9]|3[0-1])\\/(01|0[3-9]|1[0-2])\\/(20[0-9][0-9]))|((0[1-9]|1[0-9]|2[0-8]|29)\\/02\\/(20[0-9][0-9]))"), TypeofData.valueOf("String"));
+        createServiceController.addAttribute(Long.valueOf("48"),  Description.valueOf("a48"),  Description.valueOf("Fundamentação do pedido "),  Description.valueOf("label48"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
+
+        createServiceController.saveForm(Description.valueOf(formName),Description.valueOf(scriptName), pedido);
+        Calendar date = Calendar.getInstance();
+        date.set(2022,3,2);
+        //Task1
+        this.formController = new CreateFormController();
+        formController.addAttribute(Long.valueOf("49"),  Description.valueOf("a49"),  Description.valueOf("Decisao"),  Description.valueOf("label49"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("50"),  Description.valueOf("a50"),  Description.valueOf("Tento fundamentando a decisao"),  Description.valueOf("label50"),  Description.valueOf("([a-zA-Z0-9]+ *)*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("51"),  Description.valueOf("a51"),  Description.valueOf("Percentagem de Desconto"),  Description.valueOf("label51"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("52"),  Description.valueOf("a52"),  Description.valueOf("Valor de Desconto"),  Description.valueOf("label52"),  Description.valueOf("[0-9]*"), TypeofData.valueOf("String"));
+        formController.addAttribute(Long.valueOf("53"),  Description.valueOf("a53"),  Description.valueOf("Data Limite"),  Description.valueOf("label53"),  Description.valueOf("((0[1-9]|1[0-9]|2[0-9]|3[0-1])\\/(01|0[3-9]|1[0-2])\\/(20[0-9][0-9]))|((0[1-9]|1[0-9]|2[0-8]|29)\\/02\\/(20[0-9][0-9]))"), TypeofData.valueOf("String"));
+        Form at1 = formController.saveForm(Description.valueOf("ex4"),Description.valueOf("script2"),aprovacao);
+        Responsable responsable1 = new Responsable(Long.valueOf("1"), lstCollabs.get(0), new Delegaction(Long.valueOf("1"), Description.valueOf("Justificacao"), Designation.valueOf("Alternativa")), teams().get(0));
+        Task t1 = addManualTaskController.addManualTask(new Deadline(date), 3,responsable1, Description.valueOf("Comentarioex1"), Description.valueOf("Decisaoex1"), at1, new ArrayList<>());
+
+        Map<Long, Task> map = new HashMap<>();
+        List<Sequence> lstSeq = new ArrayList<>();
+        map.put(Long.valueOf("1"), t1);
+
+        //Task out
+        Task automaticTask= createServiceController.saveAutomaticTask(date, 3, "C:/ola/ola.exe");
+        this.formController = new CreateFormController();
+        formController.addAttribute(Long.valueOf("54"),  Description.valueOf("a54"),  Description.valueOf("Dias de férias já gozados no ano"),  Description.valueOf("label54"),  Description.valueOf("[0-9]{1,2}"), TypeofData.valueOf("String"));
         Form at3 = formController.saveForm(Description.valueOf("ex3"),Description.valueOf("script3"),resolucao);
 
 
