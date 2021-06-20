5b99519b099552484ae6e4040dafec4027603278
Jose Teixeira <1150462@isep.ipp.pt>
2021-06-20 04:31:22 +0100

    L2S20-74 #application taskmanagement/application/AddManualTaskController updated

--- base.core/src/main/java/eapli/base/taskmanagement/application/AddManualTaskController.java
+++ base.core/src/main/java/eapli/base/taskmanagement/application/AddManualTaskController.java
@@ -23,7 +23,7 @@ public class AddManualTaskController {
     private final TeamRepository teamRepository = PersistenceContext.repositories().team();
 
     public ManualTask addManualTask(Deadline deadline, Integer priority,
-                                    Responsable collaborator, Description commentary, Description decision, Form form, Answer lstResposta ) {
+                                    Responsable collaborator, Description commentary, Description decision, Form form, List<String> lstResposta ) {
         ManualTask manualTask = null;
         try {
             manualTask = new ManualTask(TaskState.PENDING, deadline, priority,
